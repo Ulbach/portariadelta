a3a1a968f3852636abe7c0659de1a42f91b4b68a
Ulbach <124060988+Ulbach@users.noreply.github.com>
2026-02-27 08:56:06 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
+  // O ponto garante que os arquivos sejam encontrados em qualquer subpasta do GitHub
   base: './', 
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+    }
   },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    // Garante que o build limpe a pasta anterior antes de criar uma nova
+    emptyOutDir: true,
   }
 });
