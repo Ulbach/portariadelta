c17c8247a22fd578e2f7d206cc00b0a7747bbdc4
Ulbach <124060988+Ulbach@users.noreply.github.com>
2026-02-27 09:08:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  // Use './' para garantir que os caminhos sejam relativos ao local de hospedagem
-  base: './', 
+  // Troque para o nome exato do seu repositório entre barras
+  base: '/portariadelta/', 
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
+    },
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
